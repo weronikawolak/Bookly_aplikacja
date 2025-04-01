23f5c84dc630be0fcc296b0b4fc3e6afa7a1c181
weronikawolak <weronika.wolak@op.pl>
2025-04-01 16:04:42 +0200

    text changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,62 +1,8 @@
-// import "../styles.css";
-// import React, { useState } from "react";
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-
-// const Login = () => {
-//     const [username, setUsername] = useState("");
-//     const [password, setPassword] = useState("");
-//     const [error, setError] = useState("");
-//     const navigate = useNavigate();
-
-//     const handleLogin = async (e) => {
-//         e.preventDefault();
-//         setError("");
-
-//         try {
-//             const response = await axios.post(
-//                 "http://127.0.0.1:8000/api/login/",
-//                 { username, password },
-//                 { headers: { "Content-Type": "application/json" } }
-//             );
-
-//             console.log("Server response:", response.data);
-
-//             if (response.data.token && response.data.user_id) {
-//                 localStorage.setItem("token", response.data.token);
-//                 localStorage.setItem("userId", response.data.user_id);
-
-//                 console.log("Token zapisany:", localStorage.getItem("token"));
-
-//                 navigate(`/home/${response.data.user_id}`);
-//             } else {
-//                 setError("Błąd logowania. Brak tokena.");
-//             }
-//         } catch (error) {
-//             setError("Invalid username or password");
-//             console.error("Login error:", error.response?.data);
-//         }
-//     };
-
-//     return (
-//         <div className="login-container">
-//             <h2>Login</h2>
-//             {error && <p className="error-message">{error}</p>}
-//             <form onSubmit={handleLogin}>
-//                 <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-//                 <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-//                 <button type="submit">Login</button>
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default Login;
 import "../styles.css";
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Login.css"; // nowy plik stylów
+import "./Login.css"; 
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -93,7 +39,7 @@ const Login = () => {
         <h2>Welcome back to your reading world</h2>
         {error && <p className="error-message">{error}</p>}
         <form onSubmit={handleLogin}>
-          <label>Email</label>
+          <label>Login</label>
           <input
             type="text"
             value={username}
