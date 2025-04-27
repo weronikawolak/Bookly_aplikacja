158c9fab680d8b77cd90e14da503b4da90df7eee
weronikawolak <weronika.wolak@op.pl>
2025-04-27 12:15:26 +0200

    feat(auth): configure authentication and access control

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,5 +1,5 @@
-from rest_framework import permissions
 from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
 class IsOwnerOrAdmin(permissions.BasePermission):
