c498c90b8f0d71a9eb023283c8882552c6d4eb2d
weronikawolak <weronika.wolak@op.pl>
2025-05-29 11:42:49 +0200

    refactor(api): update backend routes for new features

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import BookViewSet, ReviewViewSet, CategoryViewSet, RegisterUserView, LoginUserView, LogoutUserView, UserDetailView
+from api.views import BookViewSet, ReviewViewSet, RegisterUserView, LoginUserView, LogoutUserView, UserDetailView
 from api.views import UserBooksView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from api.views import ReadingGoalViewSet
+# from api.views import set_reading_goal, reading_goal_progress
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='books')
 router.register(r'reviews', ReviewViewSet, basename='reviews')
-router.register(r'categories', CategoryViewSet, basename='categories')
+# router.register(r'categories', basename='categories')
+router.register(r'reading-goal', ReadingGoalViewSet, basename='readinggoal')
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
@@ -31,5 +34,6 @@ urlpatterns = [
     path('api/logout/', LogoutUserView.as_view(), name="logout"),
     path('api/user/', UserDetailView.as_view(), name='user-detail'),
     path('api/user/books/', UserBooksView.as_view(), name='user-books'),
-
+    # path('api/reading-goal/', set_reading_goal),
+    # path('api/reading-goal/progress/', reading_goal_progress),
 ]
