48a527e54f0e6bd3c7cfc0697f7ee89e3de2e87b
weronikawolak <weronika.wolak@op.pl>
2025-05-29 11:42:57 +0200

    refactor(book): cleanup and improve book service logic

--- backend/api/services/book_service.py
+++ backend/api/services/book_service.py
@@ -9,3 +9,12 @@ class BookService:
     @staticmethod
     def create_book(user, validated_data):
         return BookRepository.create_book(user, validated_data)
+
+    def update_book(book, validated_data):
+            for attr, value in validated_data.items():
+                setattr(book, attr, value)
+
+            # wymuszenie aktualizacji czasu
+            book.updated_at = timezone.now()
+            book.save()
+            return book
