8ea33d0f3a444e0ea1bcafe9d94d04b69f55b2c2
weronikawolak <weronika.wolak@op.pl>
2025-06-02 12:21:24 +0200

    chore: minor adjustments to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.css"; // Importujemy CSS globalnie
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./styles.css"; // Importujemy CSS globalnie
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import "./styles.css"; // Importujemy CSS globalnie
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
